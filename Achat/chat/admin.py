a58eb232c2b04d6a0d43f5e028e8c804cc10059c
Jobaer Ahamed <jobaer.ahamed@brainstation-23.com>
2024-05-29 12:40:20 +0600

    add chat button

--- Achat/chat/admin.py
+++ Achat/chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Message, Room
 
 # Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['id','sent_by','body','created_at','created_by']
+
+admin.site.register(Room)
